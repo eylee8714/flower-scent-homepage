25ad16e4a014d7b6cb306b4a3b13715b61475405
바질브런치 <basil.brunch@bajilbeuleonchiui-MacBookAir.local>
2024-07-02 15:00:05 +0900

    [TECH-2] 코드포맷팅 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main>
-      <div>등억꽃향</div>
-    </main>
-  );
+  return <main></main>;
 }
