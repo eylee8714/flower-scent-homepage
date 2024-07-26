3b20c9722e312a26c1f6596ff8e90b18cee00f77
바질브런치 <basil.brunch@bajilbeuleonchiui-MacBookAir.local>
2024-07-26 11:46:13 +0900

    TECH-4 레이아웃 오류 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image';
+'use client';
 
 import { Building } from '@/components/home/building';
 import { Info } from '@/components/home/info';
-import { Test } from '@/components/home/test';
 import { Bread } from '@/components/home/bread';
 import { Arthall } from '@/components/home/arthall';
-import { News } from '@/components/home/news';
 import { Alliance } from '@/components/home/alliance';
-import { Order } from '@/components/home/order';
 import React from 'react';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
